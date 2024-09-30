7db933604a0bac752db635c004c3b2c45edcd7c2
vinicius <fritzenvinicius@gmail.com>
2024-09-30 20:38:18 -0300

    Remove class files

---
+++ Semana04/ExemploRelacionais.java
@@ -0,0 +1,26 @@
+public class ExemploRelacionais {
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+
+        // AND
+        System.out.println(a && b); // false
+
+        // OR
+        System.out.println(a || b); // true
+
+        // XOR
+        System.out.println(a ^ b); // true
+
+        // BITWISE AND
+        System.out.println(5 & 3); // BITWISE AND
+
+        // BITWISE OR
+        System.out.println(5 | 3); // BITWISE OR
+
+        // BITWISE XOR
+        System.out.println(5 ^ 3); // BITWISE XOR
+
+    }
+}
---
+++ Semana04/ExemploSwitch.java
@@ -0,0 +1,25 @@
+
+public class ExemploSwitch {
+    public static void main(String[] args) {
+        
+        // switch vs if-else
+        
+        // switch: utilizado para comparar valores de uma variável com valores constantes. Vantagens: mais legível
+        
+        // if-else: utilizado para comparar valores de uma variável com expressões lógicas. Vanatagens: mais flexível
+
+        int op = 2;
+
+        switch (op) {
+            case 1:
+                
+                break;
+            case 2:
+
+                break;
+            default:
+                break;
+        }
+
+    }    
+}
